bb1f218c9bcf59500c86fbfca3a2d77fc5765526
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-17 00:45:04 +0300

    feature:This will add search dunction

--- src/app/pages/github/users/users.component.ts
+++ src/app/pages/github/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/classes/user';
+import { GithubService } from 'src/app/services/github.service';;
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  loading: boolean = false;
+  showResults: boolean = false;
+  users: User[] = [];
+  searchTerm: string = '';
+  constructor(  private githubService: GithubService) { }
 
   ngOnInit(): void {
   }
+  search() {
+    // this.loading = true;
+    this.showResults = false;
+    this.users = [];
 
+    this.githubService.searchUsers(this.searchTerm).subscribe(
+      (response: any) => {
+        this.loading = false;
+        this.showResults = true;
+        for (let item of response.items) {
+          this.users.push(this.githubService.makeUserObject(User, item));
+        }
+      });
+  }
 }
