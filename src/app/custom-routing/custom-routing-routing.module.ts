b28a75a7dddff4ec956428da8018871b8f775bb4
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-17 07:48:11 +0300

    fix:fixing the router

--- src/app/custom-routing/custom-routing-routing.module.ts
+++ src/app/custom-routing/custom-routing-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomesComponent } from '../pages/homes/homes.component';
+import { ReposComponent } from '../pages/github/repos/repos.component';
+import { GithubComponent } from '../pages/github/github.component';
+import { RepoviewComponent } from '../pages/github/repos/repoview/repoview.component';
+import { UsersComponent } from '../pages/github/users/users.component';
+// import { HomesComponent } from '../pages/homes/homes.component';
+import { UserviewComponent } from '../pages/github/users/userview/userview.component';
 
 const routes: Routes = [
   {
-    path: 'github',
-    component: HomesComponent
+    path: '',
+    component: GithubComponent,
+    children: [
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: 'users/:username',
+        component: UserviewComponent
+      },
+      {
+        path: 'repositories',
+        component: ReposComponent,
+      },
+      {
+        path: 'repositories/:username/:reponame',
+        component: RepoviewComponent
+      }
   },
 ];
 
