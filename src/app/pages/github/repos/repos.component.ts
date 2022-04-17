3d5984ba0b2b54630f8ebc63ff5fb1a1690d9200
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-17 09:46:18 +0300

    fx:This will bind the ropoview

--- src/app/pages/github/repos/repos.component.ts
+++ src/app/pages/github/repos/repos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-repos',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReposComponent implements OnInit {
 
-  constructor() { }
+  username: string = ''
+  reponame: string = ''
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.username = params['username'];
+      this.reponame = params['reponame'];
+    });
   }
 
 }
