89932d35cb8cfb8fe003ad37dea24a197935f797
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-17 09:27:15 +0300

    feature:This will bind the user to userview

--- src/app/pages/github/users/userview/userview.component.ts
+++ src/app/pages/github/users/userview/userview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-userview',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserviewComponent implements OnInit {
 
-  constructor() { }
+  username: string = 'isaac-kiptoo-kilimo';
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.username = params['username'];
+    });
   }
 
 }
