8019dd04b4da50a2ebae5d3cd53fe5c1fa105ab1
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-04-18 08:26:30 +0300

    Auto-generated commit [ci skip]

--- src/app/interfaces/stat.ts
+++
@@ -1,4 +0,0 @@
-export interface Stat {
-    title: string;
-    value: any;
-}
--- src/app/pipes/github.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GithubPipe } from './github.pipe';
-
-describe('GithubPipe', () => {
-  it('create an instance', () => {
-    const pipe = new GithubPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
